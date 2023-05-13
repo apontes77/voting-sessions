f3b0b2a7a38a7cf5294cc291549c3f69ffa06f83
Alexandre <alexandrepontesgyn@gmail.com>
2023-05-13 17:37:59 -0300

    first commit

---
+++ src/main/java/com/softdesign/votingsessions/VotingsessionsApplication.java
@@ -0,0 +1,13 @@
+package com.softdesign.votingsessions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingsessionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingsessionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softdesign/votingsessions/VotingsessionsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.softdesign.votingsessions;
+
+import org.junit.jupiter.api.Test;
+
+class VotingsessionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
